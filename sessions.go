0d13e34e5bd83f202309449767a05b5c1755507b
ChelinTsien <jun54555@gmail.com>
2020-01-14 16:25:17 +0800

    Update sessions.go
    miss method

--- sessions.go
+++ sessions.go
@@ -42,6 +42,8 @@ type Session interface {
 	Options(Options)
 	// Save saves all sessions used during the current request.
 	Save() error
+        // Get session
+	Session() *sessions.Session
 }
 
 func Sessions(name string, store Store) gin.HandlerFunc {
