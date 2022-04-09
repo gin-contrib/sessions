a8b1c6c0372157065e5714f870283a22ac288d49
bos-hieu <12520839@gm.uit.edu.vn>
2022-04-09 16:51:09 +0700

    add mongo driver and upgrade go version to 1.18

---
+++ mongo/mongodriver/mongodriver_test.go
@@ -0,0 +1,56 @@
+package mongodriver
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"testing"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/tester"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const mongoTestServer = "localhost:27017"
+
+var newStore = func(_ *testing.T) sessions.Store {
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoTestServer))
+	if err != nil {
+		panic(err)
+	}
+
+	if err := client.Connect(context.Background()); err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.Background()); err != nil {
+			panic(err)
+		}
+	}()
+
+	c := client.Database("test").Collection("sessions")
+	return NewStore(c, 3600, true, []byte("secret"))
+}
+
+func TestMongo_SessionGetSet(t *testing.T) {
+	tester.GetSet(t, newStore)
+}
+
+func TestMongo_SessionDeleteKey(t *testing.T) {
+	tester.DeleteKey(t, newStore)
+}
+
+func TestMongo_SessionFlashes(t *testing.T) {
+	tester.Flashes(t, newStore)
+}
+
+func TestMongo_SessionClear(t *testing.T) {
+	tester.Clear(t, newStore)
+}
+
+func TestMongo_SessionOptions(t *testing.T) {
+	tester.Options(t, newStore)
+}
+
+func TestMongo_SessionMany(t *testing.T) {
+	tester.Many(t, newStore)
+}
